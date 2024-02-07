2ee01059e9230c2d38779882d792d5a2e2906cc1
mohamed khalid <mohamed.khalid.gamal@gmail.com>
2024-02-07 03:12:20 +0200

    fix empty field bug

--- main.js
+++ main.js
@@ -6,18 +6,21 @@ let arr = [];
 for (let i = 1; i <= window.localStorage.length; i++) {
   arr = window.localStorage.getItem("theContainer");
   arr = arr.split(",");
+  console.log(arr);
   if (arr.length > 0) {
     for (let i = 0; i < arr.length; i++) {
-      let task = document.createElement("div");
-      task.classList.add("task");
-      let pr = document.createElement("p");
-      pr.innerHTML = `${arr[i]}`;
-      let but = document.createElement("button");
-      but.classList.add("delete");
-      but.innerHTML = "delete";
-      task.appendChild(pr);
-      task.appendChild(but);
-      taskContainer.appendChild(task);
+      if (arr[i] != "") {
+        let task = document.createElement("div");
+        task.classList.add("task");
+        let pr = document.createElement("p");
+        pr.innerHTML = `${arr[i]}`;
+        let but = document.createElement("button");
+        but.classList.add("delete");
+        but.innerHTML = "delete";
+        task.appendChild(pr);
+        task.appendChild(but);
+        taskContainer.appendChild(task);
+      }
     }
   }
 }
@@ -44,7 +47,11 @@ document.addEventListener("click", function (e) {
     for (let i = 0; i < arr.length; i++) {
       if (target.previousElementSibling.innerHTML === arr[i]) {
         arr.splice(i, 1);
-        window.localStorage.setItem("theContainer", arr);
+        if (arr.length == 0) {
+          window.localStorage.clear();
+        } else {
+          window.localStorage.setItem("theContainer", arr);
+        }
         break;
       }
     }
